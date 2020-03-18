cfbffada302d2d288bdc4e6086996038b2834c3b
roshan1718 <roshanshinde1717@gmail.com>
2020-03-18 16:43:13 +0530

    ADDED code to partTime employee wage

--- employeeWage.sh
+++ employeeWage.sh
@@ -8,8 +8,11 @@ echo "Welcome To Employee Wage Calculation"
 
 #variables
 isPresent=1
-wagePerHour=20
+wagePerHr=20
 hrsPerDay=8
+isPartTime=1
+isFillTime=2
+empHrs=0
 
 #Use_Case_1
 function attendance()
@@ -21,17 +24,36 @@ function attendance()
 		echo "Employee Is Absent"
 	fi
 }
-attendance
+#attendance
 
 #Use_Case_2
 function dailyWage()
 {
 	if [[ $((RANDOM%2)) -eq $isPresent ]]
 	then
-		dailySalary=$(($wagePerHour * $hrsPerDay ))
+		dailySalary=$(($wagePerHr * $hrsPerDay ))
 		echo "Salary is.."$dailySalary
 	else
 		echo "Salary = 0"
 	fi
 }
-dailyWage
+#dailyWage
+
+#Use_Case_3
+function checkPartTime()
+{
+	if [[ $((RANDOM%3)) -eq $isPartTime ]]
+	then
+		echo "Employee Is Part Time"
+		empHrs=4
+	elif [[ $((RANDOM%3)) -eq $isFulltime ]]
+	then
+		echo "Employee is Full Time"
+		empHrs=8
+	else
+		empHrs=0
+	fi
+	dailySalary=$(($empHrs * $wagePerHr))
+	echo "Salary is.."$dailySalary
+}
+checkPartTime
