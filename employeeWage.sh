0c0f7e870c7a704d83774129cd32112be6152af5
Roshan B. Shinde <61683505+roshan1718@users.noreply.github.com>
2020-03-19 15:33:04 +0530

    UPDATE code to change spacing

--- employeeWage.sh
+++ employeeWage.sh
@@ -66,17 +66,17 @@ function usingCase()
 {
 	random=$((RANDOM%3))
 	case $random in
-			1)
-				dailySalary=$(($partTimeHrs * $wagePerHr ))
-				echo "Salary is.."$dailySalary
-			;;
-			2)
+		1)
+		dailySalary=$(($partTimeHrs * $wagePerHr ))
+		echo "Salary is.."$dailySalary
+		;;
+		2)
          	dailySalary=$(($fullTimeHrs * $wagePerHr ))
          	echo "Salary is.."$dailySalary
-         ;;
-			*)
-				echo "Salary is.. 0"
-			;;
+         	;;
+		*)
+		echo "Salary is.. 0"
+		;;
 	esac
 }
 #usingCase
